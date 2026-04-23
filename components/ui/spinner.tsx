f4fa3155531c151bfea979c81faca247bf467c9b
Jonash Marcelino <thewwwnash@gmail.com>
2026-04-23 18:06:24 +0800

    fix: type in spinner

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,9 +1,17 @@
 import { cn } from "@/lib/utils"
-import { RiLoaderLine } from "@remixicon/react"
+import { RiLoaderLine, type RemixiconComponentType } from "@remixicon/react"
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({
+  className,
+  ...props
+}: RemixiconComponentType & { className?: string }) {
   return (
-    <RiLoaderLine role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
+    <RiLoaderLine
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      {...props}
+    />
   )
 }
 
